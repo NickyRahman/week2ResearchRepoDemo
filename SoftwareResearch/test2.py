35b4930b3620aa0f19a392a3af5eaf4c77240689
Nekhat Azam <110507439+NickyRahman@users.noreply.github.com>
2022-09-16 23:02:08 +1200

    Create test2.py

---
+++ SoftwareResearch/test2.py
@@ -0,0 +1,12 @@
+#Run this file in the same folder as all the grpahic python files
+
+import os
+os.system('python ./Random_circles.py')
+os.system('python ./PatternCircle.py')
+os.system('python ./CircleHue.py')
+os.system('python ./PatternHexagon.py')
+os.system('python ./SpiralHue.py')
+os.system('python ./SpiroHue.py')
+os.system('python ./Stars.py')
+os.system('python ./CircleSpiro.py')
+os.system('python ./FractalTree.py')
